7dcb22de9f20cd05b9df590a5e7681b2f2c30574
Lucas Dietl <lucasgermandietl@luxoft.com>
2023-10-12 15:50:38 +0200

    Basic set up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'solelu-6a898',
+    appId: '1:754598418874:web:dd1c04885883bdd32c6583',
+    storageBucket: 'solelu-6a898.appspot.com',
+    apiKey: 'AIzaSyBg8SpR_v9KNX39dWrsZY_DcMeljS3RkJc',
+    authDomain: 'solelu-6a898.firebaseapp.com',
+    messagingSenderId: '754598418874',
+  },
   production: false
 };
 
